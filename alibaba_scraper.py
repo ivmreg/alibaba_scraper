35cd43154492d7a6c295b0b7f32a22199ad503dd
Engines-screaming <karl.ravago@gmail.com>
2017-02-22 22:38:58 -0800

    added moq column

--- alibaba_scraper.py
+++ alibaba_scraper.py
@@ -9,6 +9,7 @@ res = requests.get(url)
 #List of items
 item_list = []
 price_list = []
+moq_list = []
 
 #Parse all items
 soup = BeautifulSoup(res.text, 'html.parser')
@@ -20,9 +21,12 @@ for itemclass in soup.find_all('h2', class_='title'):
 for price in soup.find_all('div', class_='price'):
 	price_list.append(price.text)
 
+for moq in soup.find_all('div', class_='min-order'):
+	moq_list.append(moq.text)
+
 #Join items and prices together
-item_price_list = zip(item_list, price_list)
+item_price_list = zip(item_list, price_list, moq_list[1:])
 
 #Put list of tuples into a dataframe and export as csv
-df = pd.DataFrame(item_price_list, columns=['item', 'price'])
+df = pd.DataFrame(item_price_list, columns=['item', 'price', 'moq'])
 df.to_csv('alibaba_itemlist.csv', index=False)
